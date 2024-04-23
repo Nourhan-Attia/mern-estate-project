f77024bffdbff481420e0b60d7a7df6632cbec7c
Nourhan Attia <nourhanattia2803@gmail.com>
2024-04-23 14:33:23 +0300

    created test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,4 @@
+ const test = (req, res) => {
+  res.json({ message: "hellllllllllooooooooooooooooo" });
+};
+export default test
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js'
 dotenv.config();
 const app = express();
 mongoose.connect(process.env.MONGO).then(()=>console.log(`connected to mongo`)).catch((err)=>console.log(err));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}!`));
+app.use('/api/user',userRouter)
