a1247238541ce4b6a269d280b10653419ebf3070
Nourhan Attia <nourhanattia2803@gmail.com>
2024-04-23 09:06:43 +0300

    initialized pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
-
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
 const App = () => {
-  return (
-    <div className="text-red-500">App</div>
-  )
-}
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
