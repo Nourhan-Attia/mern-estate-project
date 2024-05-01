dabc95910240ccd44059651d4a9bddb86c5bcdec
Nourhan Attia <nourhanattia2803@gmail.com>
2024-05-01 10:06:11 +0300

    create user update api route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
@@ -8,18 +9,21 @@ const app = express();
 mongoose
   .connect(process.env.MONGO_2)
   .then(() => console.log(`connected to mongo`))
+  .then(() => {
+    app.listen(PORT, () => console.log(`server running on port ${PORT}!`));
+  })
   .catch((err) => console.log(err));
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-app.listen(PORT, () => console.log(`server running on port ${PORT}!`));
+app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
-  })
+  });
 });
