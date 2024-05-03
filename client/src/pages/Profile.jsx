95112daaafb9a7c93c5c395cb20c999e1ce74d0b
Nourhan Attia <nourhanattia2803@gmail.com>
2024-05-03 08:17:15 +0300

    completed delete user lisitng functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -127,6 +127,26 @@ const Profile = () => {
       setShowListingsError(true);
     }
   };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) => {
+        prev.filter((listing) => {
+          return listing._id !== listingId;
+        });
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
@@ -225,7 +245,9 @@ const Profile = () => {
         <p>{showListingsError ? "Error showing Listings" : ""}</p>
         {userListings && userListings.length > 0 && (
           <div className="flex flex-col gap-4">
-            <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+            <h1 className="text-center mt-7 text-2xl font-semibold">
+              Your Listings
+            </h1>
             {userListings.map((listing) => {
               <div
                 key={listing._id}
@@ -245,7 +267,14 @@ const Profile = () => {
                   <p>{listing.name}</p>
                 </Link>
                 <div className="flex flex-col items-center">
-                  <button className="text-red-700 uppercase">Delete</button>
+                  <button
+                    onClick={() => {
+                      handleListingDelete(listing._id);
+                    }}
+                    className="text-red-700 uppercase"
+                  >
+                    Delete
+                  </button>
                   <button className="text-green-700 uppercase">edit</button>
                 </div>
               </div>;
